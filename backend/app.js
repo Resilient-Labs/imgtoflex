de73b8e401a8a238f5ccf3381274f55c2174dd1d
ZrybeaWhitfield <zeathebeast@gmail.com>
2024-09-05 23:39:54 -0400

    DB initialized

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use(bodyParser.json());
+
+mongoose.connect(MONGO_URI).then(() => console.log("Connected to DB")).catch(error => console.log(error));
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
