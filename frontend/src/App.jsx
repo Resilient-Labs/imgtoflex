cc62a21eca52974999186b0f0d641a1a44890c6e
Kevin Gray <kevingray.dev@gmail.com>
2024-09-07 08:38:49 -0700

    add aiService and imageService to validate image type and process image base64 encoding and integrate w/ APP logic and buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { AIService } from "./services/aiService";
+import { ImageService } from "./services/imageService";
+
+const imageService = new ImageService();
+const aiService = new AIService();
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [getCodeIsVisibile, setGetCodeIsVisible] = useState(false);
+  const [imageType, setImageType] = useState("");
 
   const handleImageChange = (event) => {
     setSelectedImage(event.target.files[0]);
@@ -23,17 +30,24 @@ function App() {
     formData.append("image", selectedImage);
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/upload`, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name: selectedImage.name}),
-      });
+      // TODO: Add image validation for file type
+      // anthropic only accepts 4 image file types
+      setImageType(imageService.validateImage(selectedImage));
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/upload`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name: selectedImage.name }),
+        }
+      );
 
       if (response.ok) {
         const responseData = await response.json();
-        console.log("Response from server: ", responseData)
+        console.log("Response from server: ", responseData);
+        setGetCodeIsVisible(true);
         alert("Image uploaded successfully!");
       } else {
         alert("Image upload failed.");
@@ -42,17 +56,31 @@ function App() {
       console.error("Error uploading image:", error);
       alert("Error uploading image.");
     }
+
+    const handleGetCode = (event) => {
+      const processedImage = imageService.preProcessImage(selectedImage);
+      aiService.getCode((imageType = imageType), (imageData = processedImage));
+    };
   };
   return (
     <>
       <h1>Image to Code</h1>
+      <span>
+        Upload your image below. The "Get Code" buttonw will appear after the
+        image is successfully uploaded
+      </span>
       <form onSubmit={handleSubmit}>
         <input type="file" accept="image/*" onChange={handleImageChange} />
         <button type="submit">Upload Image</button>
       </form>
+      <button
+        onClick={handleGetCode}
+        display={getCodeIsVisibile ? "block" : "none"}
+      >
+        Get Code
+      </button>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
