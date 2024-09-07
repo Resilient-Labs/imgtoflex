79659757a5c02a827127c22599e88a487592f4ad
ZrybeaWhitfield <zeathebeast@gmail.com>
2024-09-07 01:07:19 -0400

    FE- added FormData obj to handle image upload, BE- installed multer, modifed '/upload' post api to handle imgs/files Resolves #1

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import multer from 'multer';
 import Prompt from './models/promptSchema.js';
 
 const app = express();
@@ -10,16 +11,26 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(cors());
 app.use(express.json());
 
+const upload = multer({ dest: 'imageUploads/' });
 mongoose.connect(MONGO_URI).then(() => console.log("Connected to DB")).catch(error => console.log(error));
 
-app.post('/upload', async (req,res) => {
-    const newImage = req.body;
-    console.log("here is our image: ", newImage);
+app.post('/upload',upload.single('file') ,async (req,res) => {
+    const { file } = req; //Image file for S3 integration
+    const { imageName, imageType, imageSize } = req.body; //other data
 
-    const prompt = new Prompt({imgName: newImage.name});
+    /* TODO: 
+    1. Remove logs once image data and prompt are stored properly
+    2. Remove local storage of images in imageUploads
+    */
+    console.log('File:', file);
+    console.log('Image Name:', imageName);
+    console.log('Image Type:', imageType);
+    console.log('Image Size:', imageSize);
+
+    const prompt = new Prompt({imgName: imageName});
     await prompt.save();
 
-    res.status(200).json({message: "Image name received", image: newImage})
+    res.status(200).json({message: "Image received", name: imageName})
 })
 
 app.listen(PORT, () => {
