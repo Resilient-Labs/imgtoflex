640dbb96b3209b82c8bbcd6a9f44c4a0fa0a6369
Kevin Gray <kevingray.dev@gmail.com>
2024-09-07 17:21:39 -0700

    working anthropic api returns basic css for image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useEffect, useState } from "react";
+import { useState } from "react";
+// // import reactLogo from "./assets/react.svg";
+// // import viteLogo from "/vite.svg";
 import "./App.css";
 import { AIService } from "./services/aiService";
 import { ImageService } from "./services/imageService";
@@ -11,7 +12,8 @@ const aiService = new AIService();
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [getCodeIsVisibile, setGetCodeIsVisible] = useState(false);
-  const [imageType, setImageType] = useState("");
+  const [imageFormat, setImageFormat] = useState("");
+  const [layoutResponse, setLayoutResponse] = useState("");
 
   const handleImageChange = (event) => {
     setSelectedImage(event.target.files[0]);
@@ -32,53 +34,61 @@ function App() {
     try {
       // TODO: Add image validation for file type
       // anthropic only accepts 4 image file types
-      setImageType(imageService.validateImage(selectedImage));
-      const response = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/upload`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ name: selectedImage.name }),
-        }
-      );
+      setImageFormat(await imageService.validateImage(selectedImage));
+      // const response = await fetch(
+      //   `${import.meta.env.VITE_BACKEND_URL}/upload`,
+      //   {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify({ name: selectedImage.name }),
+      //   }
+      // );
 
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log("Response from server: ", responseData);
-        setGetCodeIsVisible(true);
-        alert("Image uploaded successfully!");
-      } else {
-        alert("Image upload failed.");
-      }
+      // if (response.ok) {
+      //   const responseData = await response.json();
+      //   console.log("Response from server: ", responseData);
+      setGetCodeIsVisible(true);
+      //     alert("Image uploaded successfully!");
+      //   } else {
+      //     alert("Image upload failed.");
+      //   }
     } catch (error) {
       console.error("Error uploading image:", error);
       alert("Error uploading image.");
     }
-
-    const handleGetCode = (event) => {
-      const processedImage = imageService.preProcessImage(selectedImage);
-      aiService.getCode((imageType = imageType), (imageData = processedImage));
-    };
   };
+
+  const handleGetCode = async () => {
+    const processedImage = await imageService.preProcessImage(selectedImage);
+    const code = await aiService.getCode(imageFormat, processedImage);
+    console.log("layout code", code);
+    setLayoutResponse(code);
+  };
+
   return (
     <>
       <h1>Image to Code</h1>
       <span>
-        Upload your image below. The "Get Code" buttonw will appear after the
-        image is successfully uploaded
+        Upload your image below. Click the *Get Code* button that will appear
+        after the image is successfully uploaded.
       </span>
       <form onSubmit={handleSubmit}>
         <input type="file" accept="image/*" onChange={handleImageChange} />
-        <button type="submit">Upload Image</button>
+        <button display="block" type="submit">
+          Upload Image
+        </button>
       </form>
       <button
+        style={{
+          display: getCodeIsVisibile ? "inline" : "none",
+        }}
         onClick={handleGetCode}
-        display={getCodeIsVisibile ? "block" : "none"}
       >
         Get Code
       </button>
+      <div>{layoutResponse}</div>
     </>
   );
 }
